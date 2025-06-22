7033dc2b445d0686b0208adfd9492f0d6a350052
Enri Torres <85582961+enriquito11@users.noreply.github.com>
2025-06-22 02:37:00 -0300

    Add dark textured background and refine styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="bg-neutral-950 text-white selection:bg-yellow-400 selection:text-black">
+    <div className="bg-neutral-950 bg-texture-dark text-white selection:bg-yellow-400 selection:text-black">
       <div className="absolute top-0 z-[-2] h-full min-h-screen w-full bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       
       <Header />
